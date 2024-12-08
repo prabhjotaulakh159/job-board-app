a40b54617423991d69bed6a9621453f54cf4cadb
PrabhjotAulakh12345 <prabhjotaulakh68@gmail.com>
2024-12-08 00:46:11 -0500

    Make new spring app for server

---
+++ server/src/main/java/dev/prabhjot/jobpostingapp/JobpostingappApplication.java
@@ -0,0 +1,13 @@
+package dev.prabhjot.jobpostingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpostingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpostingappApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/prabhjot/jobpostingapp/JobpostingappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prabhjot.jobpostingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpostingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
